ba7ff0e1f1e9134de4fb8eb76f45f31a9cc61c05
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-15 15:16:17 +0300

    ft-create customer app

--- pooler/urls.py
+++ pooler/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('car.urls'))
+    path('', include('car.urls')),
+    path('', include('customer.urls'))
 ]
