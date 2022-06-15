e6fa4dfd702bb2401682b3f51ff15071943d12d1
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-06-15 23:25:43 +0300

    ft:define user registration form

--- pooler/settings.py
+++ pooler/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'car',
-    'customer'
+    'customer',
+    'crispy_forms',
+    'bootstrap4',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
